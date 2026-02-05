33a65b1c6e3bae88dbe5a95e775e530dee2bd3e2
Bidipta Gogoi <gogoibidipta@gmail.com>
2026-02-05 23:28:01 +0530

    work

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -1,8 +1,6 @@
-
 import { createStackNavigator } from '@react-navigation/stack';
 import ChantingScreen from '../screens/ChantingScreen';
 import DailyDarshanScreen from '../screens/DailyDarshanScreen';
-import FestivalScreen from '../screens/FestivalScreen';
 import HomeScreen from '../screens/HomeScreen'; // Assuming this exists or will be main landing
 import LanguageSelectionScreen from '../screens/LanguageSelectionScreen';
 import MantraSelectionScreen from '../screens/MantraSelectionScreen';
@@ -27,7 +25,6 @@ const AppNavigator = ({ initialRouteName = 'LanguageSelection' }) => {
             <Stack.Screen name="LanguageSelection" component={LanguageSelectionScreen} />
             <Stack.Screen name="DailyDarshan" component={DailyDarshanScreen} />
             <Stack.Screen name="Home" component={HomeScreen} />
-            <Stack.Screen name="Festivals" component={FestivalScreen} />
             <Stack.Screen name="Slokas" component={SlokasScreen} />
             <Stack.Screen name="MantraSelection" component={MantraSelectionScreen} />
             <Stack.Screen name="Gallery" component={GalleryScreen} />
